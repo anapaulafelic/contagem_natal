bea1018855a905af3fb73dc09e4ef0f652ecba7f
Ana Feliciano <ana.feliciano@cdn.com.br>
2022-03-05 17:08:43 -0300

    Melhorias no css

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function contagemNatal() {
   const hoje = new Date();
   const natal = new Date(hoje.getFullYear(), 11, 25);
   const um_dia = 1000 * 60 * 60 * 24; //milisegundos
-  const divRetorno = document.querySelector('div.contagem');
+  const divRetorno = document.querySelector('.contagem p.dias');
 
   if (hoje.getMonth() == 11 && hoje.getDate() > 25)
     natal.setFullYear(natal.getFullYear() + 1);
@@ -13,5 +13,5 @@ function contagemNatal() {
     (natal.getTime() - hoje.getTime()) / (um_dia)  
   );
   
-  divRetorno.innerHTML = `Faltam ${dias} dia(s) para o Natal!`;
+  divRetorno.innerHTML = `${dias} dia(s)`;
 }
